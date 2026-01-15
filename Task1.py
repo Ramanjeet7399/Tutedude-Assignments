c58a924533915933754033e8110cb0f68907108c
Ramanjeet7399 <bobby7399@gmail.com>
2026-01-15 08:33:57 +0530

    Task1

---
+++ Task1.py
@@ -0,0 +1,7 @@
+First_num=int(input("Enter the first number: "))
+Second_num=int(input("Enter the second number: "))
+Sum=First_num+Second_num
+Sub=First_num-Second_num
+Product=First_num*Second_num    
+Div=First_num/Second_num
+print("The sum is",Sum,"\nThe difference of two numbers is",Sub,"\nThe product is ",Product,"\nThe division is",Div) 
